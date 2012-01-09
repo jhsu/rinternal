c619dc82c9c6bd513a59e5adb46162b821c04e7d
Joseph Hsu <jhsu.x1@gmail.com>
2012-01-08 20:54:19 -0500

    whoops parm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    uid = request.env["omniaut.aut"]["uid"]
+    uid = request.env["omniaut.auth"]["uid"]
     @user = User.find_or_create_by_provider_and_uid(params[:provider], uid)
   end
 end
