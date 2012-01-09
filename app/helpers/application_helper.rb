fa4f1cb6c895ac87effef3caa68d398a6a386a8e
Joseph Hsu <jhsu.x1@gmail.com>
2012-01-09 10:59:32 -0500

    basic current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     decorated_object = "#{decorator.to_s.camelize}Decorator".constantize.new(instance)
     yield decorated_object
   end
+
+  def current_user
+    @current_user
+  end
 end
