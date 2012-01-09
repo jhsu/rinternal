9ca05bb3b242f407758227d92a25406dc6c7dd00
Joseph Hsu <jhsu.x1@gmail.com>
2012-01-09 15:56:49 -0500

    creating submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,8 +3,17 @@ class SubmissionsController < ApplicationController
     @submission = SubmissionDecorator.new(Submission.new)
   end
 
+  def show
+    @submission = SubmissionDecorator.new(Submission.find(params[:id]))
+    render :text => @submission.title
+  end
+
   def create
     type = params[:submission].delete(:type)
-    @submission = Submission.create_type(type, params[:submission])
+    if @submission = Submission.create_type(type, params[:submission])
+      redirect_to submission_path(@submission)
+    else
+      render :edit
+    end
   end
 end
