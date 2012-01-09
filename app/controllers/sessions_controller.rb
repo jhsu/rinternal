c6ee49359c3a0dca78844bbfa24486482c7e5c73
Joseph Hsu <jhsu.x1@gmail.com>
2012-01-09 01:29:26 -0500

    uid is a string

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    uid = request.env["omniauth.auth"]["uid"]
+    uid = request.env["omniauth.auth"]["uid"].to_s
     @user = User.find_or_create_by_provider_and_uid(params[:provider], uid)
   end
 end
