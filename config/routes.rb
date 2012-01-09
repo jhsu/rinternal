ecc6c86388114e2d8648a7094eaee89628f38e01
Joseph Hsu <jhsu.x1@gmail.com>
2012-01-09 18:19:20 -0500

    use correct controller for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rinternal::Application.routes.draw do
   resources :submissions
 
   match '/account/username', to: 'sessions#set_username', as: 'set_username'
-  match '/auth/force/:username', to: 'session#force'
+  match '/auth/force/:username', to: 'sessions#force'
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/auth/failure', to: 'sessions#failure'
   match '/logout', to: 'sessions#destroy'
